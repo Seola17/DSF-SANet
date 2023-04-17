a3f7b95e41249a77ed9cf174859485b5f0c0936e
seola <seola_kim@ziovision.co.kr>
2023-04-17 18:06:02 +0900

    training loss replaced with binary loss and metric

--- src/experiment/training.py
+++ src/experiment/training.py
@@ -15,8 +15,11 @@ def train_classifier(log_dir, x_train, x_train_5min, y_train, x_val, x_val_5min,
 
     classifier = create_classifier()
 
-    classifier.compile(loss=keras.losses.CategoricalCrossentropy(), optimizer=keras.optimizers.Adam(learning_rate),
-                       metrics=[keras.metrics.CategoricalAccuracy()])
+    classifier.compile(loss=keras.losses.BinaryCrossentropy(), optimizer=keras.optimizers.Adam(learning_rate),
+                    metrics=[keras.metrics.BinaryAccuracy()])
+
+    # classifier.compile(loss=keras.losses.CategoricalCrossentropy(), optimizer=keras.optimizers.Adam(learning_rate),
+    #                    metrics=[keras.metrics.CategoricalAccuracy()])
     classifier.summary()
     filepath = log_dir + '/checkpoint/classifier.weights.best.hdf5'
     checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True,
